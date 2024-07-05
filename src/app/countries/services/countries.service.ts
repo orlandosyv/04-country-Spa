4d5597f98c03440293c209ae223b929b05364c8a
OrlandoYV <sergioyv@gmail.com>
2024-07-05 12:10:17 -0500

    oy - country interface and httpClient

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountriesService {
+
+  private apiURL: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) { }
+
+  //asdf
+  searchCapital(term: string): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiURL}/capital/${term}`);
+  }
+
+}
