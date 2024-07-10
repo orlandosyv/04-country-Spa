4cda9c0b3c6ab16d3864cabce686e8ad16c73cdb
OrlandoYV <sergioyv@gmail.com>
2024-07-10 16:06:42 -0500

    oy - keep data from countries searched

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { Country } from '../../interfaces/country';
+import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
-
-type Region = "Africa" | "Americas" | "Asia" | "Europe" | "Oceania";
-
+import { Region } from '../../interfaces/region.type';
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,13 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, delay, map, Observable, of, tap } from 'rxjs';
-import { Country } from '../interfaces/country';
+import { Country } from '../interfaces/country.interface';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   private apiURL: string = 'https://restcountries.com/v3.1';
+  public cacheStore: CacheStore = {
+    byCapital: { term: '', countries: [] },
+    byCountries: { term: '', countries: [] },
+    byRegion: { region: '', countries: [] },
+  };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log('CountriesService Init');
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiURL}/capital/${term}`;
@@ -36,14 +44,12 @@ export class CountriesService {
 
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiURL}/alpha/${code}`;
-    return this.http.get<Country[]>(url)
-      .pipe(
-        map(countries => countries.length > 0 ? countries[0] : null),
-        catchError(error => {
-          console.log(error);
-          return of(null);
-          }
-        )
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError((error) => {
+        console.log(error);
+        return of(null);
+      })
     );
   }
 }
